ba5b0b6216977f67d9b6bf4fb71c47eba8020ff7
Kevin Beijer <kevinbeijer1996@gmail.com>
2019-07-26 13:12:43 +0200

    Click + Drag
    adg

--- Restaurant Rush/Assets/GameController.cs
+++ Restaurant Rush/Assets/GameController.cs
@@ -4,22 +4,5 @@ using UnityEngine;
 
 public class GameController : MonoBehaviour
 {
-    public float timeIncrease = 2;
-    public bool timeElapsed = false;
 
-    public int items;
-
-    private float timeRemaining;
-
-    void Start()
-    {
-        //The timer, as a child of this gameobject, receive this and start the countdown using the timeRemaining variable
-        BroadcastMessage("Start Timer", timeRemaining);
-    }
-
-    //If the game controller receives this signal from the timer, it will end the game
-    void timeHasElapsed()
-    {
-        timeElapsed = true; // game over 
-    }
 }
